2e9f569c0ce4f9978a859dc16e2d2cb469ddfc38
Nepul Kahandawa <nepulkahandawa7f@gmail.com>
2026-08-20 19:17:20 -0400

    Update service

--- base/service.py
+++ base/service.py
@@ -7,7 +7,7 @@ client = genai.Client(api_key=settings.GEMINI_API_KEY)
 def ask_gemini(prompt, paragraphs, level):
     try:
         response = client.models.generate_content(
-            model="gemini-2.5-flash",
+            model="gemini-3.6-flash",
             contents=f"""
                     Answer clearly in a standard essay format.
 
